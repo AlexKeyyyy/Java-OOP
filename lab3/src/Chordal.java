2dcd77772932d33a9fd428acec67c4de8a0c13c5
”Alex <koba.ayu@edu.spbstu.ru>
2023-11-25 01:56:33 +0300

    shit again 2

--- lab3/src/Chordal.java
+++ lab3/src/Chordal.java
@@ -1,7 +1,2 @@
 public class Chordal {
-    public Chordal()
-    {
-        //System.out.println("Chordal constructor was made");
-
-    }
 }
--- lab3/src/Lynx.java
+++ lab3/src/Lynx.java
@@ -1,6 +1,2 @@
 public class Lynx extends CatFamily{
-    public Lynx()
-    {
-        //System.out.println("Lynx constructor was made");
-    }
 }
